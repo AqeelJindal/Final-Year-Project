a661c410ac6141fffefb15d80f7586fe0376ec0a
AqeelJindal <aqeeljindal123@gmai.com>
2026-03-13 17:17:00 +0000

    Modified ga.py: Lecture assignment in progress

--- ga.py
+++ ga.py
@@ -20,7 +20,6 @@ TUTORIAL_GROUPS = 9
 PERSONAL_TUTORIAL_GROUPS = 27
 
 
-
 def create_groups(module_teachers, all_teachers, n_groups, teacher_load, max_groups):
     groups = {}
 
@@ -115,7 +114,6 @@ nL_lec = len(lecture_events)
 #
 # # print(lab_events)
 
-# Bookmark: assign lab groups properly
 
 # TUTORIALS
 
@@ -141,308 +139,320 @@ for module_name, module_data in Modules.items():
 
 nL_tut = len(tutorial_events)
 
-print(tutorial_events)
-print(nL_tut)
+# print(tutorial_events)
+# print(nL_tut)
 
 # # PERSONAL TUTORIALS
 #
 # per_tut_events = []
 #
+# personal_groups_per_tutorial = 3
+#
 # for module_name, module_data in Modules.items():
+#
+#     n_groups = len(module_data["tutorials"])
+#     tutorials_per_lab = n_groups // n_lab_groups
+#
 #     for group_name, info in module_data["Personal Tutorials"].items():
-#         per_tut_events.append({
-#             "type": "personal tutorial",
-#             "module": module_name,
-#             "group": group_name,
-#             "teacher": info["teacher"]
-#         })
+#
+#         tutorial_group = int(group_name.replace("Group", ""))
+#
+#         lab_group = (tutorial_group - 1) // tutorials_per_lab + 1
+#
+#         for p in range(1, personal_groups_per_tutorial + 1):
+#
+#             personal_group = (tutorial_group - 1) * personal_groups_per_tutorial + p
+#
+#             per_tut_events.append({
+#                 "type": "personal tutorial",
+#                 "module": module_name,
+#                 "lab_group": lab_group,
+#                 "tutorial_group": tutorial_group,
+#                 "personal_tutorial_group": personal_group,
+#                 "teacher": info["teacher"]
+#             })
 #
 # nL_per_tut = len(per_tut_events)
-#
-# # print(per_tut_events)
-#
-# # UNIFIED EVENT LIST
-# all_events = (
-#         lecture_events
-#         # + lab_events
-#         + tutorial_events
-#     # + per_tut_events
-# )
-#
-# n_events = len(all_events)
-#
-# # print(all_events)
-#
-# # TIMESLOTS
-#
-# event_duration = {
-#     "lecture": 2,
-#     "tutorial": 1,
-#     # "lab": 1,
-#     # "personal tutorial": 1
-# }
-#
-# DAYS = ["MON", "TUE", "WED", "THU", "FRI"]
-# HOURS = list(range(9, 18))  # 9 to 17
-#
-# nG = len(DAYS) * len(HOURS)
-#
-#
-# # to later compute (day, hour)
-# def decode_slot(slot_index, event):
-#     duration = event_duration[event["type"]]
-#
-#     day_index = slot_index // len(HOURS)
-#     hour_index = slot_index % len(HOURS)
-#
-#     day = DAYS[day_index]
-#     start = HOURS[hour_index]
-#
-#     return f"{day} {start}:00-{start + duration}:00"
-#
-#
-# # so that sessions with more than 1 hr slot get assigned correctly
-# def valid_slots(duration):
-#     slots = []
-#
-#     for slot in range(nG):
-#         hour_index = slot % len(HOURS)
-#
-#         if hour_index <= len(HOURS) - duration:
-#             slots.append(slot)
-#
-#     return slots
-#
-#
-# valid_start_slots = {}
-#
-# for e_type, duration in event_duration.items():
-#     valid_start_slots[e_type] = valid_slots(duration)
-#
-#
-# # GLOBAL CLASH MATRIX
-#
-# C = np.zeros((n_events, n_events), dtype=np.int64)
-#
-# for i in range(n_events):
-#     for j in range(n_events):
-#         if i == j:
-#             continue
-#
-#         event_i = all_events[i]
-#         event_j = all_events[j]
-#
-#         module_i = event_i["module"]
-#         module_j = event_j["module"]
-#
-#         teacher_i = event_i["teacher"]
-#         teacher_j = event_j["teacher"]
-#
-#         type_i = event_i["type"]
-#         type_j = event_j["type"]
-#
-#         group_i = event_i["group"]
-#         group_j = event_j["group"]
-#
-#         # # RULE 1: Labs cannot be parallel
-#         # if type_i == "lab" and type_j == "lab":
-#         #     C[i][j] = HARD_PENALTY
-#
-#         # RULE 2: Same modules cannot overlap (except Tutorials)
-#         if module_i == module_j:
-#             if type_i == "tutorial" and type_j == "tutorial":
-#                 C[i][j] = 0
-#             else:
-#                 C[i][j] = HARD_PENALTY
-#
-#         # RULE 3: Different modules cannot be parallel (except Personal tutorials and where students are from seperate groups)
-#         if module_i != module_j:
-#             C[i][j] = HARD_PENALTY
-#             # if type_i == "personal tutorial" and type_j == "personal tutorial":
-#             #     C[i][j] = 0
-#             if type_i == "tutorial" and type_j == "tutorial":
-#                 if group_i != group_j:
-#                     C[i][j] = 0
-#
-#
-#         # RULE 4: Same teacher cannot teach two events at the same time
-#         if teacher_i == teacher_j:
-#             C[i][j] = HARD_PENALTY
-#
-# # print(C)
-#
-# # TEACHER TIME PREFERENCES
-#
-# teacher_time_preferences = {}
-#
-# event_types = ["lecture", "lab", "tutorial", "personal tutorial"]
-#
-# for teacher in all_teachers:
-#     teacher_time_preferences[teacher] = {}
-#
-#     for e_type in event_types:
-#         teacher_time_preferences[teacher][e_type] = [0] * nG
-#
-# # print(teacher_time_preferences)
-#
-# # Soft rules
-# for slot in range(nG):
-#     day_index = slot // len(HOURS)
-#     hour_index = slot % len(HOURS)
-#
-#     day = DAYS[day_index]
-#     hour = HOURS[hour_index]
-#
-#     # Amy prefers lecture before 12
-#     if hour >= 12:
-#         teacher_time_preferences["Amy"]["lecture"][slot] = SOFT_SMALL
-#
-#     # Amy prefers tutorials after 12
-#     if hour < 12:
-#         teacher_time_preferences["Amy"]["tutorial"][slot] = SOFT_SMALL
-#
-#     # Natasha prefers morning lectures
-#     if hour >= 11:
-#         teacher_time_preferences["Natasha"]["lecture"][slot] = SOFT_SMALL
-#
-#     # Sarah prefers no tutorials on THU
-#     if day == "THU":
-#         teacher_time_preferences["Sarah"]["tutorial"][slot] = SOFT_SMALL
-#
-# # print(teacher_time_preferences)
-#
-# # STUDENT TIME PREFERENCES
-#
-# student_time_preferences = {}
-#
-# # Students as one entity
-# student_time_preferences["ALL_STUDENTS"] = {}
-#
-# for e_type in event_types:
-#     student_time_preferences["ALL_STUDENTS"][e_type] = [0] * nG
-#
-# # print(student_time_preferences)
-#
-# for slot in range(nG):
-#     day_index = slot // len(HOURS)
-#     hour_index = slot % len(HOURS)
-#
-#     day = DAYS[day_index]
-#     hour = HOURS[hour_index]
-#
-#     # Students prefer lectures/tutorial before 17:00
-#     if hour >= 17:
-#         student_time_preferences["ALL_STUDENTS"]["lecture"][slot] = SOFT_MEDIUM
-#         student_time_preferences["ALL_STUDENTS"]["tutorial"][slot] = SOFT_MEDIUM
-#
-#     # Students prefer no tutorials on Fri
-#     if day == "FRI":
-#         student_time_preferences["ALL_STUDENTS"]["tutorial"][slot] = SOFT_MEDIUM
-#
-#     # Students prefer afternoon lectures
-#     if hour > 15 or hour < 12:
-#         student_time_preferences["ALL_STUDENTS"]["lecture"][slot] = SOFT_MEDIUM
-#
-#
-# # print(student_time_preferences)
-#
-#
-# # GENETIC ALGORITHM
-#
-# # CHROMOSOME (LIST OF SLOT INDICES)
-# # A chromosome: list of timeslot assignments for all the events
-#
-# def random_timetable():
-#     timetable = []
-#
-#     for event in all_events:
-#         e_type = event["type"]
-#         timetable.append(random.choice(valid_start_slots[e_type]))
-#
-#     return timetable
-#
-#
-# # print(n_events)
-# # print(len(random_timetable()))
-#
-# # FITNESS FUNCTION
-#
-# def fitness(timetable):
-#     # Penalties separated by type
-#
-#     hard_penalty = 0
-#     tutorial_soft = 0
-#     # lecture_soft = 0
-#     # lab_soft = 0
-#     # personal_soft = 0
-#
-#     # Clash
-#     for i in range(n_events):
-#         for j in range(i + 1, n_events):
-#
-#             slot_i = timetable[i]
-#             slot_j = timetable[j]
-#
-#             dur_i = event_duration[all_events[i]["type"]]
-#             dur_j = event_duration[all_events[j]["type"]]
-#
-#             slots_i = set(range(slot_i, slot_i + dur_i))
-#             slots_j = set(range(slot_j, slot_j + dur_j))
-#
-#             if slots_i & slots_j:
-#                 hard_penalty += C[i][j]
-#
-#     # Time preferences
-#     for i, event in enumerate(all_events):
-#         teacher = event["teacher"]
-#         e_type = event["type"]
-#         slot = timetable[i]
-#
-#         teacher_pen = teacher_time_preferences[teacher][e_type][slot]
-#         student_pen = student_time_preferences["ALL_STUDENTS"][e_type][slot]
-#
-#         total_soft = teacher_pen + student_pen
-#
-#         if e_type == "tutorial":
-#             tutorial_soft += total_soft
-#
-#         # elif e_type == "lecture":
-#         #     lecture_soft += total_soft
-#         #
-#         # elif e_type == "lab":
-#         #     lab_soft += total_soft
-#         #
-#         # elif e_type == "personal tutorial":
-#         #     personal_soft += total_soft
-#
-#     # # Sequential Constraints # fix this for (O(n^2))
-#     # for i, event_i in enumerate(all_events):
-#     #     for j, event_j in enumerate(all_events):
-#     #
-#     #         if event_i["module"] == event_j["module"]:
-#     #
-#     #             # two sessions of same tutorial group must be conducted after a certain amount of break in between based on lecture timings (later modify to make one tutorial happen after one lecture is done)
-#     #
-#     #
-#     #             # Tutorial must come before lecture
-#     #             if event_i["type"] == "tutorial" and event_j["type"] == "lecture":
-#     #                 if timetable[i] >= timetable[j]:
-#     #                     tutorial_soft += SOFT_MEDIUM
-#
-#     return \
-#         (
-#             hard_penalty,
-#             tutorial_soft,
-#             # lecture_soft,
-#             # lab_soft,
-#             # personal_soft
-#         )
-#
-#
-# # print(fitness(random_timetable()))
-#
-# # GENETIC OPERATORS
-#
+# print(per_tut_events)
+#
+# UNIFIED EVENT LIST
+all_events = (
+        lecture_events
+        # + lab_events
+        + tutorial_events
+    # + per_tut_events
+)
+
+n_events = len(all_events)
+
+# print(all_events)
+
+# TIMESLOTS
+
+event_duration = {
+    "lecture": 2,
+    "tutorial": 1,
+    # "lab": 1,
+    # "personal tutorial": 1
+}
+
+DAYS = ["MON", "TUE", "WED", "THU", "FRI"]
+HOURS = list(range(9, 18))  # 9 to 17
+
+nG = len(DAYS) * len(HOURS)
+
+
+# to later compute (day, hour)
+def decode_slot(slot_index, event):
+    duration = event_duration[event["type"]]
+
+    day_index = slot_index // len(HOURS)
+    hour_index = slot_index % len(HOURS)
+
+    day = DAYS[day_index]
+    start = HOURS[hour_index]
+
+    return f"{day} {start}:00-{start + duration}:00"
+
+
+# so that sessions with more than 1 hr slot get assigned correctly
+def valid_slots(duration):
+    slots = []
+
+    for slot in range(nG):
+        hour_index = slot % len(HOURS)
+
+        if hour_index <= len(HOURS) - duration:
+            slots.append(slot)
+
+    return slots
+
+
+valid_start_slots = {}
+
+for e_type, duration in event_duration.items():
+    valid_start_slots[e_type] = valid_slots(duration)
+
+# GLOBAL CLASH MATRIX
+
+C = np.zeros((n_events, n_events), dtype=np.int64)
+
+for i in range(n_events):
+    for j in range(n_events):
+        if i == j:
+            continue
+
+        event_i = all_events[i]
+        event_j = all_events[j]
+
+        module_i = event_i["module"]
+        module_j = event_j["module"]
+
+        teacher_i = event_i["teacher"]
+        teacher_j = event_j["teacher"]
+
+        type_i = event_i["type"]
+        type_j = event_j["type"]
+
+        # # RULE 1: Labs cannot be parallel
+        # if type_i == "lab" and type_j == "lab":
+        #     C[i][j] = HARD_PENALTY
+
+        # RULE 2: Tutorial clashes
+        if type_i == "tutorial" and type_j == "tutorial":
+
+            tut_i = event_i["tutorial_group"]
+            tut_j = event_j["tutorial_group"]
+
+            # Same tutorial group cannot overlap
+            if tut_i == tut_j:
+                C[i][j] = HARD_PENALTY
+            else:
+                C[i][j] = 0
+
+        # RULE 3: Same teacher cannot teach two events at the same time
+        if teacher_i == teacher_j:
+            C[i][j] = HARD_PENALTY
+
+        # RULE 4: Lectures clash with everything
+        if type_i == "lecture" or type_j == "lecture":
+            C[i][j] = HARD_PENALTY
+
+# print(C)
+
+# TEACHER TIME PREFERENCES
+
+teacher_time_preferences = {}
+
+event_types = ["lecture", "lab", "tutorial", "personal tutorial"]
+
+for teacher in all_teachers:
+    teacher_time_preferences[teacher] = {}
+
+    for e_type in event_types:
+        teacher_time_preferences[teacher][e_type] = [0] * nG
+
+# print(teacher_time_preferences)
+
+# Soft rules
+for slot in range(nG):
+    day_index = slot // len(HOURS)
+    hour_index = slot % len(HOURS)
+
+    day = DAYS[day_index]
+    hour = HOURS[hour_index]
+
+    # Amy prefers lecture before 12
+    if hour >= 12:
+        teacher_time_preferences["Amy"]["lecture"][slot] = SOFT_SMALL
+
+    # Amy prefers tutorials after 12
+    if hour < 12:
+        teacher_time_preferences["Amy"]["tutorial"][slot] = SOFT_SMALL
+
+    # Natasha prefers morning lectures
+    if hour >= 11:
+        teacher_time_preferences["Natasha"]["lecture"][slot] = SOFT_SMALL
+
+    # Sarah prefers no tutorials on THU
+    if day == "THU":
+        teacher_time_preferences["Sarah"]["tutorial"][slot] = SOFT_SMALL
+
+# print(teacher_time_preferences)
+
+# STUDENT TIME PREFERENCES
+
+student_time_preferences = {}
+
+# Students as one entity
+student_time_preferences["ALL_STUDENTS"] = {}
+
+for e_type in event_types:
+    student_time_preferences["ALL_STUDENTS"][e_type] = [0] * nG
+
+# print(student_time_preferences)
+
+for slot in range(nG):
+    day_index = slot // len(HOURS)
+    hour_index = slot % len(HOURS)
+
+    day = DAYS[day_index]
+    hour = HOURS[hour_index]
+
+    # Students prefer lectures/tutorial before 17:00
+    if hour >= 17:
+        student_time_preferences["ALL_STUDENTS"]["lecture"][slot] = SOFT_MEDIUM
+        student_time_preferences["ALL_STUDENTS"]["tutorial"][slot] = SOFT_MEDIUM
+
+    # Students prefer no tutorials on Fri
+    if day == "FRI":
+        student_time_preferences["ALL_STUDENTS"]["tutorial"][slot] = SOFT_MEDIUM
+
+    # Students prefer afternoon lectures
+    if hour > 15 or hour < 12:
+        student_time_preferences["ALL_STUDENTS"]["lecture"][slot] = SOFT_MEDIUM
+
+
+# print(student_time_preferences)
+
+
+# GENETIC ALGORITHM
+
+# CHROMOSOME (LIST OF SLOT INDICES)
+# A chromosome: list of timeslot assignments for all the events
+
+def random_timetable():
+    timetable = []
+
+    for event in all_events:
+        e_type = event["type"]
+        timetable.append(random.choice(valid_start_slots[e_type]))
+
+    return timetable
+
+
+# print(n_events)
+# print(len(random_timetable()))
+
+# FITNESS FUNCTION
+
+def fitness(timetable):
+    # Penalties separated by type
+
+    hard_penalty = 0
+    tutorial_soft = 0
+    lecture_soft = 0
+    # lab_soft = 0
+    # personal_soft = 0
+
+    # Clash
+    for i in range(n_events):
+        for j in range(i + 1, n_events):
+
+            slot_i = timetable[i]
+            slot_j = timetable[j]
+
+            dur_i = event_duration[all_events[i]["type"]]
+            dur_j = event_duration[all_events[j]["type"]]
+
+            slots_i = set(range(slot_i, slot_i + dur_i))
+            slots_j = set(range(slot_j, slot_j + dur_j))
+
+            if slots_i & slots_j:
+                hard_penalty += C[i][j]
+
+    # Time preferences
+    for i, event in enumerate(all_events):
+        teacher = event["teacher"]
+        e_type = event["type"]
+        slot = timetable[i]
+
+        teacher_pen = teacher_time_preferences[teacher][e_type][slot]
+        student_pen = student_time_preferences["ALL_STUDENTS"][e_type][slot]
+
+        total_soft = teacher_pen + student_pen
+
+        if e_type == "tutorial":
+            tutorial_soft += total_soft
+
+        elif e_type == "lecture":
+            lecture_soft += total_soft
+        #
+        # elif e_type == "lab":
+        #     lab_soft += total_soft
+        #
+        # elif e_type == "personal tutorial":
+        #     personal_soft += total_soft
+
+    # bookmark: modify this
+    # Sequential Constraints # fix this for (O(n^2))
+    for i, event_i in enumerate(all_events):
+        for j, event_j in enumerate(all_events):
+
+            if event_i["module"] == event_j["module"]:
+
+                # two sessions of same tutorial group must be conducted after a certain amount of break in between based on lecture timings (later modify to make one tutorial happen after one lecture is done)
+
+
+                # Tutorial must come before lecture
+                if event_i["type"] == "tutorial" and event_j["type"] == "lecture":
+                    if timetable[i] >= timetable[j]:
+                        tutorial_soft += SOFT_MEDIUM
+
+    return \
+        (
+            hard_penalty,
+            tutorial_soft,
+            # lecture_soft,
+            # lab_soft,
+            # personal_soft
+        )
+
+
+# print(fitness(random_timetable()))
+
+# GENETIC OPERATORS
+
 # # SELECTION (TOURNAMENT)
 #
 # def select(population):
@@ -587,6 +597,7 @@ print(nL_tut)
 # # print("All timetable tests passed.")
 
 # Bookmark:
+# add git.ignore
 # create a testing code to analyse the timetable like it follows all scheduling constraints (lectures, labs, PT)
 # now assign lectures, lab sessions, personal tutorials
 # better the clash preferences that already exist so that it thinks abt all sessions
